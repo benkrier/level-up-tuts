c470daa9a4464ef8902d0da2c4b007b5c5ea5637
Ben Krier <benkrier@cna-83-136.cna-17.depaul.edu>
2018-03-02 12:18:35 -0600

    added React 16 project

---
+++ lumdb-app/src/MoviesList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Movie from './Movie';
+
+
+class MoviesList extends Component {
+  state = {
+    movies: [],
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=ff88f1e65473c9df5de623291f3cb2bd&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <MovieGrid>
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+      </MovieGrid>
+    );
+  }
+}
+
+export default MoviesList;
+
+const MovieGrid = styled.div`
+  display: grid;
+  padding: 1rem;
+  grid-template-columns: repeat(6, 1fr);
+  grid-row-gap: 1rem;
+`;
