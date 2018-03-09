4aa1776572916538bc792433592f84e7c2cb1a13
Ben Krier <benkrier@cna-83-136.cna-17.depaul.edu>
2018-03-09 16:32:51 -0600

    build blog with gatsby

---
+++ levelup-blog/src/pages/about.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About Me</h1>
+        <p>Ennui palo santo locavore, echo park microdosing knausgaard prism chia fanny pack enamel pin hoodie offal. Fixie raw denim vinyl woke jianbing bushwick. Af sartorial selvage vegan. Yuccie godard aesthetic banjo butcher tumblr umami normcore activated charcoal tofu cornhole woke cloud bread mustache. Actually thundercats pok pok schlitz affogato kickstarter. Cray direct trade mumblecore cronut skateboard. Ennui mixtape pour-over kombucha kinfolk.</p>
+      </div>
+    );
+  }
+};
---
+++ levelup-blog/src/posts/post.md
@@ -0,0 +1,8 @@
+---
+title: 'Welcome to the new blog!'
+date: 'March 9, 2018'
+---
+
+Pour-over leggings try-hard wayfarers mlkshk etsy authentic letterpress trust fund iPhone normcore. Photo booth fanny pack chillwave godard keytar VHS shaman man braid portland. Blue bottle fam hashtag glossier squid occupy tofu. Cold-pressed vegan brunch PBR&B fingerstache iceland. Unicorn art party hammock, copper mug salvia chillwave pitchfork kinfolk flexitarian. Vaporware hoodie raw denim, sustainable scenester blog vexillologist man bun. Shabby chic meh austin cronut raclette pop-up lyft pabst vexillologist, succulents paleo poutine.
+
+<!-- end -->
